28842602b6892c5f3886c9bcde762849afe27a49
EricSFChu <echu6@mail.ccsf.edu>
2016-01-24 20:38:18 -0800

    Added a full page view to be implemented

---
+++ ECFlicks/FullViewHelper.swift
@@ -0,0 +1,21 @@
+//
+//  FullViewHelper.swift
+//  ECFlicks
+//
+//  Created by EricDev on 1/24/16.
+//  Copyright © 2016 EricDev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class FullViewHelper{
+    
+    var imageURLtransfer = NSURL(string: "")
+    var overviewTransfer: String = ""
+    
+    public func setImage(var something: NSURL){
+        imageURLtransfer = something
+    }
+
+}
--- Readme.md
+++ Readme.md
@@ -23,7 +23,7 @@ The following **optional** features are implemented:
 
 The following **additional** features are implemented:
 
-- [ ] List anything else that you can get done to improve the app functionality!
+- [x] Created a full view page to be implemented later
 
 ## Video Walkthrough 
 
